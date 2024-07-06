a36614f067bb197de74a9559d9df24f1906f0a52
vismodeb <vismodeb2000@gmail.com>
2024-07-06 16:48:16 +0600

    project done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,7 @@
 $(document).ready(function () {
-  // Mobile Nav Toggle
+  // Nav Toggle
   $("#toggleNav-icon").click(function () {
     $(this).toggleClass("open");
     $(".menu").slideToggle();
-  });
-  const activePage = window.location.pathname;
-  const navLinks = document.querySelectorAll("nav a").forEach((link) => {
-    if (link.href.includes(`${activePage}`)) {
-      link.classList.add("active");
-      console.log(link);
-    }
   });
 });
